0dc3e68f8c08bef5473373b7fa2534a38f7f4a91
netshedzo <67120128+netshedz@users.noreply.github.com>
2024-06-27 14:18:10 +0200

    inject the forecast html from javascript

--- src/index.js
+++ src/index.js
@@ -55,7 +55,30 @@ function searchCities(event) {
   searchingCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌥️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>15°</strong>
+      </div>
+      <div class="weather-forecast-temperature">9°</div>
+    </div>
+  </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchCities);
 
 searchingCity("paris");
+displayForecast();
